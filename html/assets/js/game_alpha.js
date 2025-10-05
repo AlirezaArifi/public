d9d5453333c0e3ff580510d9f05cfdf2df1e0259
Rere Laptop <alirezaarifi@proton.me>
2025-10-05 12:28:45 +0700

    Lets see how this goes

--- html/assets/js/game_alpha.js
+++ html/assets/js/game_alpha.js
@@ -2,7 +2,7 @@ alert("TES!!");
 var mod_one = "https://api.modrinth.com/v2/search?facets=[[%22downloads%20%3E=%20200%22],[%22project_type:mod%22]]&index=follows&offset=1&limit=1";
 const userAction = async () => {
   const mod_one_response = await fetch(mod_one);
-  const mod_one_json = await response.mod_one_response(); 
+  const mod_one_json = await mod_one_response(); 
     console.log(mod_one_json);
 }
 userAction();
